7370a7f9d96c2fbbb130b1a54f97c238aafde236
Ethan Dunham <ethan.dunham@gmail.com>
2018-01-09 14:38:45 -0500

    Update timeDisplay.py

--- cs496-MobileAndCloud/timeDisplay.py
+++ cs496-MobileAndCloud/timeDisplay.py
@@ -1,12 +1,10 @@
-from datetime import datetime
 import webapp2
-#Adapted from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/appengine/standard/hello_world/main.py
-now = datetime.now()
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        now = now.hour+":"+ now.minute+":"+ now.second
-        self.response.write(now)
+        self.response.write('Hello, World!')
 
 
 app = webapp2.WSGIApplication([
