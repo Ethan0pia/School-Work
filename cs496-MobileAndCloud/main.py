8234d89b791ae55bc7d69ab7539976c3ca71adbb
Ethan Dunham <ethan.dunham@gmail.com>
2018-01-09 14:44:28 -0500

    Update main.py

--- cs496-MobileAndCloud/main.py
+++ cs496-MobileAndCloud/main.py
@@ -1,10 +1,13 @@
 import webapp2
+from datetime import datetime
+#Adapted from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/appengine/standard/hello_world/main.py
 
-
+now = datetime.now()
+time = now.hour+":"+ now.minute+":"+ now.second
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write(time)
 
 
 app = webapp2.WSGIApplication([
