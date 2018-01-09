767100e94812148f2591bb6579790522d9ce5c64
Ethan Dunham <ethan.dunham@gmail.com>
2018-01-09 14:53:04 -0500

    Update main.py

--- cs496-MobileAndCloud/main.py
+++ cs496-MobileAndCloud/main.py
@@ -2,9 +2,9 @@ import webapp2
 from datetime import datetime
 #Adapted from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/appengine/standard/hello_world/main.py
 
-now = datetime.now()
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        now = datetime.now()
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(now)
 
