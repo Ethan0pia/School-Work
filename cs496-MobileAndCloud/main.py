0addf523eb0af3d3986458d8c2308d36522f50cc
Ethan Dunham <ethan.dunham@gmail.com>
2018-01-09 14:50:14 -0500

    Update main.py

--- cs496-MobileAndCloud/main.py
+++ cs496-MobileAndCloud/main.py
@@ -3,11 +3,10 @@ from datetime import datetime
 #Adapted from https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/appengine/standard/hello_world/main.py
 
 now = datetime.now()
-time = now.hour+":"+ now.minute+":"+ now.second
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('%s' % time)
+        self.response.write(now)
 
 
 app = webapp2.WSGIApplication([
