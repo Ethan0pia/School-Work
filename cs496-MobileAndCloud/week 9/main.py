02569c685747a68c9337bccd8b227c53d128d589
Ethan Dunham <ethan.dunham@gmail.com>
2018-03-04 00:33:27 -0500

    Add files via upload

--- cs496-MobileAndCloud/week 9/main.py
+++ cs496-MobileAndCloud/week 9/main.py
@@ -716,7 +716,7 @@ def verifyUser(access_token):
         method = urlfetch.GET,
         headers=headers)
     user_id = 0
-    json_result = json.loads(result)
+    json_result = result.to_dict()
     if 'error' not in json_result
         json_result = json.loads(result.content)
         user_id = json_result['id']
